7fb97f46474a3d3d5822d1912c7babce3453348d
visierip <visierip@omsi-software.com>
2024-02-06 16:53:38 +0100

    Initial commit

---
+++ app/src/main/java/com/omsi/softaptest/MyOnStartTetheringCallback.java
@@ -0,0 +1,14 @@
+package com.omsi.softaptest;
+
+public abstract class MyOnStartTetheringCallback {
+        /**
+         * Called when tethering has been successfully started.
+         */
+        public abstract void onTetheringStarted();
+
+        /**
+         * Called when starting tethering failed.
+         */
+        public abstract void onTetheringFailed();
+
+}
