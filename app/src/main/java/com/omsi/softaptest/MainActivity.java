6631c0f7699a0e3ba79470066eeb9902de41034b
paolovsrl <paolovsrl@gmail.com>
2024-07-18 15:46:26 +0200

    adb wifi, scrcpy-server.jar

--- app/src/main/java/com/omsi/softaptest/MainActivity.java
+++ app/src/main/java/com/omsi/softaptest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.omsi.softaptest;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,6 +8,13 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.omsi.softap.SoftApManager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +46,120 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "SoftAp is "+ (res? "active": "inactive"));
             }
         });
+
+
+        findViewById(R.id.enable_adb_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              try {
+                    Process  p = Runtime.getRuntime().exec("setprop service.adb.tcp.port 5555 ");
+                  //  p.getOutputStream().write("ls -ll".getBytes());
+                    p.waitFor();
+
+                    p = Runtime.getRuntime().exec("stop adbd ");
+                    p.waitFor();
+
+
+                    p = Runtime.getRuntime().exec("start adbd ");
+                    p.waitFor();
+
+                } catch (IOException e) {
+                    // Handle IOException (e.g., log the error)
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    // Handle InterruptedException
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+        findViewById(R.id.disable_adb_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Process  p = Runtime.getRuntime().exec("setprop service.adb.tcp.port -1 ");
+                    p.waitFor();
+
+                    p = Runtime.getRuntime().exec("stop adbd ");
+                    p.waitFor();
+
+
+                    p = Runtime.getRuntime().exec("start adbd ");
+                    p.waitFor();
+
+                } catch (IOException e) {
+                    // Handle IOException (e.g., log the error)
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    // Handle InterruptedException
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        findViewById(R.id.copy_library_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AssetManager assetManager = getAssets();
+                try {
+                    InputStream inputStream = assetManager.open("scrcpy-server.jar");
+
+
+                    Process p1 = Runtime.getRuntime().exec("touch /data/sk/tmp ");
+                    p1.waitFor();
+
+                    File outputFile = new File("/data/sk/scrcpy-server.jar");
+                   // outputFile.createNewFile();
+                    FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+
+                    inputStream.close();
+                    outputStream.close();
+
+
+                    Process p = Runtime.getRuntime().exec("chmod 777 /data/sk/scrcpy-server.jar ");
+                    p.waitFor();
+                    //su", "chmod", "777", "/data/local/tmp/$targetFileName";
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    private void executeShellCmd(String cmd){
+        Process process = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder output = new StringBuilder();
+
+        File workingDirectory = new File("/data/local/tmp");
+        String[] environment = {"PATH=/system/bin"};
+
+        try {
+            process = Runtime.getRuntime().exec(cmd,  environment, workingDirectory);
+            bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line = "";
+            while((line = bufferedReader.readLine() )!= null){
+                output.append(line);
+            }
+            process.waitFor();
+            Log.d(TAG, "Result: "+output.toString());
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
